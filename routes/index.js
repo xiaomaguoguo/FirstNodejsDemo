dc1bbfe6d57c1359bf2764de8620e1715fc66bb0
KNothing <mzh3344258@163.com>
2017-12-04 23:23:29 +0800

    学习post get请求，并返回json数据

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'KNohting Express，FK' });
 });
 
+/* Post home page.*/
+router.post('/postFuck',function (req,res) {
+    var json = {name:"KNothing",height:"1.90",age:"100"};
+    res.json(json)
+})
+
 module.exports = router;
+
+
