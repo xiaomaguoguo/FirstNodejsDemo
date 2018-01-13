3732841effc392797d7e9026c55b070869bdbb29
xiaomaguoguo <xiaomaguoguo@gmail.com>
2018-01-13 15:24:05 +0800

    试玩数据库操作，可以从接口返回数据给客户端了。。 哈哈哈。。 。

---
+++ config/dbconfig.js
@@ -0,0 +1,16 @@
+module.exports = {
+    // aliyun内网
+    // mysql: {
+    //     host     : 'rm-2zeck4365zi746evc.mysql.rds.aliyuncs.com',
+    //     user     : 'root',
+    //     password : 'Gjw920623',
+    //     database : 'robin_common_server'
+    // },
+    // 阿里云外网
+    mysql: {
+        host     : 'localhost',
+        user     : 'root',
+        password : 'mzh3344258',
+        database : 'nodejs'
+    },
+}
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,30 @@
 var express = require('express');
+var mysql       = require('mysql');
 var router = express.Router();
+var dbconfig    = require('../config/dbconfig');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/', function(req, res, next) {
+  // res.send('respond with a resource');
+    // RDS查询
+    let connection = mysql.createConnection(dbconfig.mysql);
+    let sql = "select * from user as a left join userinfo as b on a.username = b.username;";
+    connection.connect();
+    connection.query(sql, function (err, result) {
+        if (err != null) {
+            res.json({error: err.toString() });
+            return;
+        }
+        if (result.length === 0) {
+            res.json({ error: '用户名或密码错误！' });
+        } else {
+            const user = result[0];
+            // req.session.sign = true;
+            // req.session.user_id = user.userId;
+            res.json({ data: result });
+        }
+    });
+    connection.end();
 });
 
 module.exports = router;
